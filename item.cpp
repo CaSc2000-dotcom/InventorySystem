2bf70735706c7f36b34830f6c667352fa55d80ea
CaSc2000-dotcom <39818295+CaSc2000-dotcom@users.noreply.github.com>
2025-10-05 23:43:40 -0600

    modified insertion overload

--- item.cpp
+++ item.cpp
@@ -12,7 +12,7 @@
 std::ostream& operator<<(std::ostream& out, const Item& item)
 {
    out << "--------------------------------------------------\n"
-       << "Name: " << item.displayName << " -- Value: " << item.value << "\n"
+       << item.displayName << " -- Value: " << item.value << "\n"
        << item.description << "\n"
        << "---\n"
        << "ItemID: " << item.itemID << " -- Icon Path: " << item.iconPath << "\n"
