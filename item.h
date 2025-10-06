0931c8d745c5c5d283063d2e856201247c471b43
CaSc2000-dotcom <39818295+CaSc2000-dotcom@users.noreply.github.com>
2025-10-05 20:22:30 -0600

    Item class implemented - Item class and three other classes that inheret from Item: Powerup, Exactor, and Potion

--- item.h
+++ item.h
@@ -1,5 +1,94 @@
+/***********************************************************************
+ * Source File:
+ *    ITEM
+ * Author:
+ *    Caleb Schmid
+ * Summary:
+ *    Base class for representing an item in the game
+ ***********************************************************************/
+
 #pragma once
-class Item
+
+#include <string>
+
+enum ItemsType
 {
+   INVULNERABLILITY, XP_MAGNET, HASTE,
+   SINGULARITY, LIGHTNING, FREEZE,
+   HEALTH, SHIELD,
+   INVALID
 };
 
+/*********************************************
+ * Item
+ * A consumable usable by the player
+ *********************************************/
+class Item
+{
+public:
+   // Constructors
+   Item(int itemID = -1, std::string displayName = "", std::string description = "",
+        std::string iconPath = "", int value = -1, float magnitude = -1.0f) 
+      : itemID(itemID), displayName(displayName), description(description), 
+        iconPath(iconPath), value(value), magnitude(magnitude) {}
+   ~Item() {}
+
+   // Getters
+   int getItemID() const { return itemID; }
+   std::string getDisplayName() const { return displayName; }
+   std::string getDescription() const { return description; }
+   std::string getIconPath() const { return iconPath; }
+   int getValue() const { return value; }
+
+   virtual ItemsType getType() const { return INVALID; }
+
+   virtual void use() {}
+
+protected:
+   int itemID;
+   std::string displayName;
+   std::string description;
+   std::string iconPath;
+   int value;
+   float magnitude;
+};
+
+/*********************************************
+ * PowerupItem
+ * Item that provides a temporary buff to the player
+ *********************************************/
+class PowerupItem : public Item
+{
+public:
+   PowerupItem(float duration = 0.0f, float magnitude = 0.0f) 
+             : duration(duration), Item() {}
+protected:
+   float duration;
+};
+
+/*********************************************
+ * ExactorItem
+ * Item that affects enemies in some way
+ *********************************************/
+class ExactorItem : public Item
+{
+public:
+   ExactorItem(float areaOfEffect = 0.0f, float magnitude = 0.0f)
+             : areaOfEffect(areaOfEffect), Item() {}
+protected:
+   float areaOfEffect;
+};
+
+/*********************************************
+ * PotionItem
+ * Item that provides a one-time benefit to the player
+ *********************************************/
+class PotionItem : public Item
+{
+public:
+   PotionItem(float areaOfEffect = 0.0f, float magnitude = 0.0f)
+      : areaOfEffect(areaOfEffect), Item() {
+   }
+protected:
+   float areaOfEffect;
+};
