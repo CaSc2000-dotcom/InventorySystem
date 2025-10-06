c9dff4661cd4836dfe0d1601cef11a09a0505af2
CaSc2000-dotcom <39818295+CaSc2000-dotcom@users.noreply.github.com>
2025-10-05 23:30:03 -0600

    Testing current items - project files updated

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 // InventorySystem.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include "item.h"
+#include "healthPotion.h"
+#include "flimsyShield.h"
 #include <iostream>
 
 int main()
 {
-   std::cout << "Hello World!";
+   std::cout << "---------------------------------------------" << std::endl;
+   std::cout << ">>>> Caleb's Rinky-Dink Inventory System <<<<" << std::endl;
+   std::cout << "---------------------------------------------" << std::endl;
+
+   HealthPotion hpot;
+   std::cout << "\n" << hpot << std::endl;
+   hpot.use();
+
+   FlimsyShield spot;
+   std::cout << "\n" << spot << std::endl;
+   spot.use();
 }
